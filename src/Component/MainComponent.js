72339b49bd1025583a8f5a6dfe6a2e3f4a1e53fd
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-09-17 14:09:06 +0100

    [Add] Footer

--- src/Component/MainComponent.js
+++ src/Component/MainComponent.js
@@ -14,6 +14,24 @@ export default function MailComponent(props){
           {/* <SearchResultComponent results={props.searchResult}/> */}
         </div>
       </div>
+      <div className="footer">
+        This project was coded by Somayeh.Mahjoob and is{" "}
+        <a
+          href="https://github.com/somayehmahjoob/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code on GitHub
+        </a>
+        , and Hosted on and{" "}
+        <a
+          href="https://amazing-wiles-4ca1c5.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify{" "}
+        </a>
+      </div>
     </div>
   );
 }
