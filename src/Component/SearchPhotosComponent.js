87b86318c2640c39110dbc7c7356633da80284f5
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-09-21 12:11:41 +0100

    [Modify] fixed the responsive image

--- src/Component/SearchPhotosComponent.js
+++ src/Component/SearchPhotosComponent.js
@@ -6,10 +6,10 @@ export default function SearchPhotosComponent(props){
   if(props.photos){
     return (
       <div className="SearchPhotosComponent">
-        <div className="box box-img">
+        <div className="row box-img">
           {props.photos.map((photo, index) => {
             return (
-              <div key={index}>
+              <div className="col-md-4 col-sm-6 mb-2" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
